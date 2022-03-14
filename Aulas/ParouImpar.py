80ac4d9b63d87ff910d51e062e0d5a90b8848054
DavidSiqueiradosSantos <davidsiqueiradossantos556@gmail.com>
2022-03-14 03:06:56 +0000

    Impar ou Par

--- Aulas/ParouImpar.py
+++ Aulas/ParouImpar.py
@@ -1,54 +1,79 @@
 from random import randint
 
-nome = str(input("Nome do jogador: "))
+nome = str(input("Informe o nome do jogador: "))
 
-b = 0
-computador = randint(0, 10)
-jogador = str(input("Escolha par ou impar: "))
+escolha = str(input("VEZ DO JOGADOR!\n'IMPAR' OU 'PAR'? "))
 
-if jogador == "par":
-    par = jogador
-    impar = computador
-    print("Computador será 'impar' ")
-    print("==" * 20)
+computador = " "
+jogador = " "
+j = 0
+c = 0
 
-elif jogador == "impar":
-    impar = jogador
-    par = computador
-    print("Computador será 'par' ")
-    print("==" * 20)
 
+if escolha == "impar" or escolha == "IMPAR" or escolha == "Impar":
+    jogador = "impar"
+    computador = "par"
+    print("O jogador {} será '{}'.".format(nome.upper(), jogador.upper()))
+    print("O Computador será '{}'.".format(computador.upper()))
+elif escolha == "par" or escolha == "PAR" or escolha == "Par":
+    jogador = "par"
+    computador = "impar"
+    print("O jogador {} será '{}'.".format(nome.upper(), jogador.upper()))
+    print("O Computador será '{}'.".format(computador.upper()))
 else:
-    b = 1
-    print("ERRO")
+    print("Erro!")
 
-if b != 1:
+cont = 1
+while cont != 6:
+    print("\nRodada", cont, "\n")
 
-    numero = int(input("Vez do Jogador {}!\nEscolha um número de (0/10): ".format(nome)))
-    while numero > 10:
-        if numero >= 10:
-            numero = int(input("Escolha um número de (0/10): "))
+    numero = int(input("VEZ DO {}!\nEscolha um número de (0/10): ".format(nome).upper()))
+    while numero > 10 or numero < 0:
+        numero = int(input("VEZ DO {}!\nEscolha um número de (0/10): ".format(nome).upper()))
 
-    print(nome + " escolheu: {}".format(numero))
-    print("\nComputador escolheu: {}".format(computador))
+    compNumero = randint(0, 10)
 
-    total = numero + computador
-
-    print("A Soma é: {}".format(total))
-    computador = "0"
+    total = numero + compNumero
 
     if total % 2 == 0:
-        print("O Resultado é: Par")
-        computador = "par"
+        print("Você jogou {}.".format(numero))
+        print("O Computador jogou {}.".format(compNumero))
+        print("O total é: {}.".format(total))
+        print(total, "é 'Par!'")
+        computador = 'par'
     else:
-        print("O Resultado é: Impar")
-        computador = "impar"
+        print("Você jogou {}.".format(numero))
+        print("O Computador jogou {}.".format(compNumero))
+        print("O total é: {}.".format(total))
+        print(total, "é Impar!")
+        computador = 'impar'
+
     if jogador == "par" and computador == "par":
         print()
-        print("VENCEU O JOGADOR: ", nome + "!")
+        print("VENCEU O JOGADOR:", nome.upper() + "!")
+        j += 1
     elif jogador == "impar" and computador == "impar":
         print()
-        print("VENCEU O JOGADOR: ", nome + "!")
+        print("VENCEU O JOGADOR:", nome.upper() + "!")
+        j += 1
     else:
         print()
         print("VENCEU O COMPUTADOR!")
+        c += 1
+
+    if j >= 3:
+        print("O jogador {}, venceu a partida com {} vitoria, escolhendo '{}'!".format(nome.upper(), j, jogador))
+        break
+    elif c >= 3:
+        print("O computador venceu a partida com {} vitoria!".format(c))
+        print("O jogador {}, perdeu a partida com {} vitoria!".format(nome.upper(), j, computador))
+        break
+
+    cont += 1
+
+
+
+
+
+
+
